ff61c3997cb6b507ff76462b5261d3583fdb97f4
mhogeveen <maarten@Maartens-Air.home>
2021-01-06 15:45:58 +0100

    Add counter functionality to intersection observer

--- script.js
+++ script.js
@@ -2,6 +2,35 @@
 const defaultAnimation = 'fadeInLeft'
 
 document.addEventListener('DOMContentLoaded', () => {
+   const animationDuration = 3000 // Duration of animation in ms
+   const frameDuration = 1000 / 60 // Calculate ms/frame at 60 frames/second
+   const totalFrames = Math.round(animationDuration / frameDuration)
+
+   const easeOutQuad = (t) => t * (2 - t)
+
+   const animateCountUp = (el) => {
+      let frame = 0
+      const countTo = parseInt(el.innerHTML, 10)
+
+      const counter = setInterval(() => {
+         frame++
+
+         const progress = easeOutQuad(frame / totalFrames)
+         const currentCount = Math.round(countTo * progress)
+
+         if (parseInt(el.innerHTML, 10) !== currentCount) {
+            el.innerHTML = currentCount
+         }
+
+         if (frame === totalFrames) {
+            clearInterval(counter)
+         }
+      }, frameDuration)
+   }
+
+   // const countupEls = document.querySelectorAll('.counter__count')
+   // countupEls.forEach(animateCountUp)
+
    // Select DOM elements to animate
    const entries = document.querySelectorAll('[data-animate]')
 
@@ -34,6 +63,9 @@ document.addEventListener('DOMContentLoaded', () => {
                const animationVariant = element.dataset.animate
                const delayModifier = Number(element.dataset.animationParent)
                handleAnimationParent(element, animationVariant, delayModifier)
+            } else if (element.dataset.animate === 'counter') {
+               element.classList.add('active')
+               animateCountUp(element)
             } else {
                element.classList.add(element.dataset.animate)
             }
